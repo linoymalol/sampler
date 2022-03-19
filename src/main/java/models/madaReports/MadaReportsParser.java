4d448a89704c93970002b2e3cb3d32192afcdcdc
Linoy Malol <linoymalol1@gmail.com>
2022-03-19 17:41:41 +0200

    Added MadaReports Parser

--- src/main/java/models/madaReports/MadaReportsParser.java
+++ src/main/java/models/madaReports/MadaReportsParser.java
@@ -1,11 +1,19 @@
-package etl.extract;
+package models.madaReports;
 
-import models.MadaReports;
+import models.madaReports.MadaReports;
 import org.apache.commons.csv.CSVRecord;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class MadaReportsParser {
-    public MadaReports MadaReports(CSVRecord rec) {
-        return new MadaReports(rec.get(0), rec.get(1), Integer.parseInt(rec.get(2)), rec.get(3), rec.get(4), rec.get(5),
-                rec.get(6), Integer.parseInt(rec.get(7)), rec.get(8), rec.get(9), rec.get(10), rec.get(11));
+    public List<MadaReports> MadaReports(List<CSVRecord> rec) {
+        List<MadaReports>madaReports = new LinkedList<>();
+        for (int i = 1; i < rec.size(); i++){
+            madaReports.add(new MadaReports(rec.get(i).get(0), rec.get(i).get(1), Integer.parseInt(rec.get(i).get(2)), rec.get(i).get(3), rec.get(i).get(4), rec.get(i).get(5),
+                    rec.get(i).get(6), Integer.parseInt(rec.get(i).get(7)), rec.get(i).get(8), rec.get(i).get(9), rec.get(i).get(10), rec.get(i).get(11)));
+        }
+        return madaReports;
     }
+
 }
