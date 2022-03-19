5679ac13a476ca205a33165de754e3a914f4359b
Linoy Malol <linoymalol1@gmail.com>
2022-03-20 01:45:15 +0200

    Added FileReader Interface

--- src/main/java/etl/extract/FileReader.java
+++ src/main/java/etl/extract/FileReader.java
@@ -1,9 +1,7 @@
 package etl.extract;
 
-import org.apache.commons.csv.CSVParser;
-
 import java.io.IOException;
 
-public interface Reader {
-    Object read(String path) throws IOException;
+public interface FileReader extends Reader {
+    Object readFromFile(String path) throws IOException;
 }
