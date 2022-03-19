49397baecd3f14b04ca66545a0f0eb0eacd53af3
Linoy Malol <linoymalol1@gmail.com>
2022-03-20 01:47:03 +0200

    Added Load interface

--- src/main/java/etl/load/Load.java
+++ src/main/java/etl/load/Load.java
@@ -1,2 +1,8 @@
-package etl.load;public interface Load {
+package etl.load;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface Load {
+    public void load(String path, List listOfObjects) throws IOException;
 }
