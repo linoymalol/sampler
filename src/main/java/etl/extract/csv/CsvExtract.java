71ea1ab05d5fce5ba4aaf757bfd9414db6e010b9
Linoy Malol <linoymalol1@gmail.com>
2022-03-20 01:42:41 +0200

    Added CsvExtract class

--- src/main/java/etl/extract/csv/CsvExtract.java
+++ src/main/java/etl/extract/csv/CsvExtract.java
@@ -1,2 +1,14 @@
-package etl.extract.csv;public class CsvExtract {
+package etl.extract.csv;
+
+import etl.extract.Extract;
+import java.io.IOException;
+import java.util.List;
+
+public class CsvExtract implements Extract {
+    CsvReader csvReader = new CsvReader();
+    CsvParser csvParser = new CsvParser();
+
+    public List extract(String path) throws IOException {
+        return csvParser.parse(csvReader.readFromFile(path));
+    }
 }
