7711db37dd5943657ff858bf418a7fa9dad498d1
Linoy Malol <linoymalol1@gmail.com>
2022-03-20 01:48:19 +0200

    Added JsonLoad class

--- src/main/java/etl/load/JsonLoad.java
+++ src/main/java/etl/load/JsonLoad.java
@@ -1,2 +1,10 @@
-package etl.load;public class JsonLoad {
+package etl.load;
+
+import java.io.IOException;
+import java.util.List;
+
+public class JsonLoad extends FileLoader {
+    public void load(String path, List listOfObjects) throws IOException {
+        super.load(path + ".json", listOfObjects);
+    }
 }
