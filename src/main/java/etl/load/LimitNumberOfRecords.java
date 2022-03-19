5a523202f4c6302684329f9a071bbfec95f4e4f7
Linoy Malol <linoymalol1@gmail.com>
2022-03-20 01:49:18 +0200

    Added LimitNumberOfRecords class

--- src/main/java/etl/load/LimitNumberOfRecords.java
+++ src/main/java/etl/load/LimitNumberOfRecords.java
@@ -1,2 +1,22 @@
-package etl.load;public class LimitNumberOfRecords {
+package etl.load;
+
+import java.util.List;
+
+public class LimitNumberOfRecords {
+    public String newPath(String path, int numberOfFile) {
+        int index = path.indexOf('.');
+        String startString = path.substring(0, index - 1);
+        String endString = path.substring(index, path.length());
+        return startString + "" + numberOfFile + endString;
+    }
+
+    public int numberOfFiles(List listOfObjects, int numberOfRecords) {
+        if (listOfObjects.size() % numberOfRecords == 0) {
+            return listOfObjects.size() / numberOfRecords;
+        }
+        return listOfObjects.size() / numberOfRecords + 1;
+    }
+
+
+
 }
