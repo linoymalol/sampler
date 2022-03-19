e7a6125797a061b12e5bc6a34a7044af0d302717
Linoy Malol <linoymalol1@gmail.com>
2022-03-20 01:50:23 +0200

    Added Main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class Main {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import etl.EtlManager;
+import etl.extract.Extract;
+import etl.extract.csv.CsvExtract;
+import etl.load.JsonLoad;
+import etl.load.Load;
+import etl.load.XmlLoad;
+import models.madaReports.MadaReports;
+import models.madaReports.MadaReportsParser;
+
+import javax.xml.crypto.dsig.Transform;
+import java.util.HashMap;
+import java.util.Properties;
+import java.io.IOException;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        //Properties properties = new Properties();
+        //madaReports = csvExtract.extract(properties.getProperty("MadaReportsCsvPath"));
+        */
+        HashMap<String, Extract> extractHashMap = new HashMap<>();
+        HashMap<String, Transform> transformHashMap = new HashMap<>();
+        HashMap<String, Load> loadHashMap = new HashMap<>();
+        CsvExtract csvExtract= new CsvExtract();
+        extractHashMap.put("csv", csvExtract);
+        JsonLoad jsonLoad = new JsonLoad();
+        loadHashMap.put("json",jsonLoad);
+        XmlLoad xmlLoad = new XmlLoad();
+        loadHashMap.put("xml", xmlLoad);
+        EtlManager etlManager = new EtlManager(extractHashMap, transformHashMap, loadHashMap);
+        try {
+            etlManager.process("src\\main\\resources\\MadaReports.csv", "madda", "json");
+            //etlManager.process("src\\main\\resources\\MadaReports.csv", "mada", "xml");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
