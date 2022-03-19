1ab8c61b8cfcc9a9d22fa193ed296ad78dc43be1
Linoy Malol <linoymalol1@gmail.com>
2022-03-20 01:47:44 +0200

    Added FileLoader abstract class

--- src/main/java/etl/load/FileLoader.java
+++ src/main/java/etl/load/FileLoader.java
@@ -1,2 +1,28 @@
-package etl.load;public interface FileLoad {
+package etl.load;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public abstract class FileLoader implements Load {
+
+    public void load(String path, List listOfObjects) throws IOException {
+        LimitNumberOfRecords limitNumberOfRecords = new LimitNumberOfRecords();
+        int numberOfFiles = limitNumberOfRecords.numberOfFiles(listOfObjects, 500);
+        int counter = 0;
+        int indexInList = 0;
+        while (counter != numberOfFiles) {
+            ObjectMapper mapper = new ObjectMapper();
+            if (500 * (counter + 1) > listOfObjects.size()) {
+                mapper.writeValue(new File(path), listOfObjects.subList(indexInList, listOfObjects.size()));
+            } else {
+                mapper.writeValue(new File(path), listOfObjects.subList(indexInList, 500 * (counter + 1)));
+            }
+            indexInList = 500 * (counter + 1);
+            counter++;
+            path = limitNumberOfRecords.newPath(path, counter);
+        }
+    }
 }
