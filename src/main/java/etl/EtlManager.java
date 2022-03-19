0479b7c879e15f5a3b9a51b4ea80ceff38b6987b
Linoy Malol <linoymalol1@gmail.com>
2022-03-20 01:50:02 +0200

    Added EtlManager class

--- src/main/java/etl/EtlManager.java
+++ src/main/java/etl/EtlManager.java
@@ -1,2 +1,49 @@
-package etl;public class EtlManager {
+package etl;
+
+import com.google.common.io.Files;
+import etl.extract.Extract;
+import etl.load.Load;
+
+import javax.xml.crypto.dsig.Transform;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+public class EtlManager {
+    private HashMap<String, Extract> extractHashMap;
+    private HashMap<String, Transform> transformHashMap;
+    private HashMap<String, Load> loadHashMap;
+
+
+    public EtlManager(HashMap<String, Extract> extractHashMap, HashMap<String, Transform> transformHashMap,
+                      HashMap<String, Load> loadHashMap){
+        this.extractHashMap = extractHashMap;
+        this.transformHashMap = transformHashMap;
+        this.loadHashMap = loadHashMap;
+    }
+
+    public List extractTheFile(String fromFile) throws IOException {
+        return extractHashMap.get(getExtensionByGuava(fromFile)).extract(fromFile);
+    }
+
+    public void loadToFile(String toDirectoryName, List listOfObjects, String typeOfFile) throws IOException {
+        createDirectoryIfNotExists("C:\\" + toDirectoryName);
+        loadHashMap.get(typeOfFile).load("C:\\" + toDirectoryName + "\\" + toDirectoryName + "1", listOfObjects);
+    }
+
+    public void  process(String fromFile, String toDirectoryName, String typeOfFile) throws IOException {
+        loadToFile(toDirectoryName, extractTheFile(fromFile), typeOfFile);
+    }
+
+    public String getExtensionByGuava(String filename) {
+        return Files.getFileExtension(filename);
+    }
+
+    public void createDirectoryIfNotExists(String path){
+        File folder = new File(path);
+        if (!folder.exists()) {
+            folder.mkdir();
+        }
+    }
 }
