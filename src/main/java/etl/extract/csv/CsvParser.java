9a945bc34a8658c0ea9c288db294aaa0c2560504
Linoy Malol <linoymalol1@gmail.com>
2022-03-20 01:46:05 +0200

    Added CsvParser class

--- src/main/java/etl/extract/csv/CsvParser.java
+++ src/main/java/etl/extract/csv/CsvParser.java
@@ -1,2 +1,16 @@
-package etl.extract.csv;public class CsvParser {
+package etl.extract.csv;
+
+import etl.extract.Parser;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.IOException;
+import java.util.List;
+
+public class CsvParser implements Parser {
+    public List<CSVRecord> parse(Object object) throws IOException {
+        CSVParser parser = (CSVParser) object;
+        List<CSVRecord> records = parser.getRecords();
+        return records;
+    }
 }
