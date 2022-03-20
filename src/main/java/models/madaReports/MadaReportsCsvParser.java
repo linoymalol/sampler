bfddcb922060a20f1c6f960aac413f4b7d484795
Linoy Malol <linoymalol1@gmail.com>
2022-03-20 12:30:50 +0200

    Changed from MadaReportsParser to MadaReportsCsvParser

--- src/main/java/models/madaReports/MadaReportsCsvParser.java
+++ src/main/java/models/madaReports/MadaReportsCsvParser.java
@@ -1,2 +1,17 @@
-package models.madaReports;public class MadaReportsCsvParser {
+package models.madaReports;
+
+import org.apache.commons.csv.CSVRecord;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class MadaReportsCsvParser {
+    public List<MadaReports> MadaReports(List<CSVRecord> rec) {
+        List<MadaReports>madaReports = new LinkedList<>();
+        for (int i = 1; i < rec.size(); i++){
+            madaReports.add(new MadaReports(rec.get(i).get(0), rec.get(i).get(1), Integer.parseInt(rec.get(i).get(2)), rec.get(i).get(3), rec.get(i).get(4), rec.get(i).get(5),
+                    rec.get(i).get(6), Integer.parseInt(rec.get(i).get(7)), rec.get(i).get(8), rec.get(i).get(9), rec.get(i).get(10), rec.get(i).get(11)));
+        }
+        return madaReports;
+    }
 }
